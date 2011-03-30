c314b1451b41530c446cd6da6a6bc7929c4f2e2a
Ruben Smits <ruben.smits@mech.kuleuven.be>
2011-03-30 20:54:06 +0200

    rtt_ros_integration: generate and use pc file
    Signed-off-by: Ruben Smits <ruben.smits@mech.kuleuven.be>

--- cmake/GenerateRTTtypekit.cmake
+++ cmake/GenerateRTTtypekit.cmake
@@ -83,8 +83,7 @@ macro(ros_generate_rtt_typekit package)
   
   orocos_typekit( rtt-ros-${package}-typekit ${CMAKE_CURRENT_SOURCE_DIR}/src/orocos/types/ros_${package}_typekit.cpp ${ROSMSG_TYPEKIT_PLUGINS})
   orocos_typekit( rtt-ros-${package}-transport ${CMAKE_CURRENT_SOURCE_DIR}/src/orocos/types/ros_${package}_transport.cpp )
-  target_link_libraries(rtt-ros-${package}-transport rtt_ros_integration-${OROCOS_TARGET})
-
+  
   set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${ROSMSG_TYPEKIT_PLUGINS};${ROSMSG_TRANSPORT_PLUGIN};${CMAKE_CURRENT_SOURCE_DIR}/src/orocos/types/ros_${package}_typekit.cpp;${CMAKE_CURRENT_SOURCE_DIR}/src/orocos/types/ros_${package}_transport.cpp")
   
 endmacro(ros_generate_rtt_typekit)
