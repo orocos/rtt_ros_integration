8946a65950b7946f64e043c3d1e7fdbcd7f04da8
Jonathan Bohren <jbo@jhu.edu>
2013-08-01 05:14:50 -0400

    moving stuff up the tree

--- typekits/rtt_std_srvs/src/rtt_ros_service_proxies.cpp
+++ typekits/rtt_std_srvs/src/rtt_ros_service_proxies.cpp
@@ -8,52 +8,96 @@
 #include <std_srvs/Empty.h>
 ////////////////////////////////////////////////////////////////////////////////
 
-class ROSServiceServerProxyBase { };
-class ROSServiceClientProxyBase { };
 
-template<class ROS_SERVICE_T, class ROS_SERVICE_REQ_T, class ROS_SERVICE_RESP_T>
-class ROSServiceServerProxy : public ROSServiceServerProxyBase {
+class ROSServiceProxyBase
+{
 public:
 
-  typedef RTT::OperationCaller<bool(ROS_SERVICE_REQ_T&, ROS_SERVICE_RESP_T&)> ProxyOperationCallerType;
-
-  ROSServiceClientProxy(RTT::TaskContext* task, const std::string &full_service_name) {
-
+  ROSServiceProxyBase(const std::string &full_service_name) 
+    : full_service_name_(full_service_name)
+  {
     // Split the full service name into (service_ns, service_name)
-    boost::iterator_range<std::string::const_iterator> service_ns_end = boost::algorithm::find_last(full_service_name, "/");
-    std::string service_ns(full_service_name.begin(), service_ns_end.begin());
-    std::string service_name(service_ns_end.begin()+1, full_service_name.end());
+    boost::iterator_range<std::string::const_iterator> service_ns_end = boost::algorithm::find_last(full_service_name_, "/");
+    service_ns_(full_service_name.begin(), service_ns_end.begin());
+    service_name_(service_ns_end.begin()+1, full_service_name.end());
 
     // Get the service namespace tokens
-    std::vector<std::string> service_ns_tokens;
-    boost::split(service_ns_tokens, service_ns, boost::is_any_of("/"));
+    boost::split(service_ns_tokens_, service_ns_, boost::is_any_of("/"));
+    
+    // Store the operation name
+    std::vector<std::string> operation_name_tokens(service_ns_tokens_);
+    operation_name_tokens.push_front(task->getName());
+    operation_name_tokens.push_back(service_name_);
+    operation_name_ = boost::algorithm::join(operation_name_tokens, ".");
+  }
 
+  //! Get the name of the Orocos operation
+  const std::string& getOperationName() const {
+    return operation_name_;
+  }
+
+private:
+
+  //! ROS Service name (fully qualified)
+  std::string full_service_name_;
+  std::string service_ns_;
+  std::string service_name_;
+  std::vector<std::string> service_ns_tokens_;
+  
+  //! Orocos operation name
+  std::string operation_name_;
+}
+
+class ROSServiceServerProxyBase : public ROSServiceProxyBase
+{ 
+public:
+  ROSServiceServerProxyBase(const std::string &full_service_name)
+    : ROSServiceProxyBase(full_service_name);
+private:
+  //! The underlying ROS service server
+  ros::ServiceServer server_;
+};
+
+template<class ROS_SERVICE_T, class ROS_SERVICE_REQ_T, class ROS_SERVICE_RESP_T>
+class ROSServiceServerProxy : public ROSServiceServerProxyBase 
+{
+public:
+  //! Operation caller for a ROS service server proxy
+  typedef RTT::OperationCaller<bool(ROS_SERVICE_REQ_T&, ROS_SERVICE_RESP_T&)> ProxyOperationCallerType;
+
+  /** \brief Construct a ROS service server and associate it with an Orocos
+   * task's required interface and operation caller.
+   */
+  ROSServiceClientProxy(RTT::TaskContext* task, const std::string &full_service_name) 
+    : ROSServiceServerProxyBase(full_service_name)
+  {
     // Add required interfaces corresponding to the namespace tokens
     RTT::ServiceRequester *required = task->requires();
-    for(std::vector<std::string>::iterator it = service_name_tokens.begin()+1;
-        it != service_name_tokens.end();
+    for(std::vector<std::string>::iterator it = service_ns_tokens_.begin();
+        it != service_ns_tokens_.end();
         ++it)
     {
       required = required->requires(*it);
     }
 
     // Create the operation caller with the name of the ROS service
-    operation_caller_.reset(new ProxyOperationCallerType(service_name));
+    operation_caller_.reset(new ProxyOperationCallerType(service_name_));
 
     // Add the operation caller to the required interface
     required->addOperationCaller(*operation_caller_.get());
 
     // Construct the ROS service server
     ros::NodeHandle nh;
-    server_ = nh.advertiseService(service_name, &ROSServiceServerProxy<ROS_SERVICE_T, ROS_SERVICE_REQ_T, ROS_SERVICE_RESP_T>::ros_service_callback, this);
+    server_ = nh.advertiseService(
+        full_service_name_, 
+        &ROSServiceServerProxy<ROS_SERVICE_T, ROS_SERVICE_REQ_T, ROS_SERVICE_RESP_T>::ros_service_callback, 
+        this);
   }
+
 private:
 
   //! The Orocos operation caller which gets called 
   boost::shared_ptr<ProxyOperationCallerType> operation_caller_;
-
-  //! The underlying ROS service server
-  ros::ServiceServer server_;
   
   //! The callback called by the ROS service server when this service is invoked
   bool ros_service_callback(ROS_SERVICE_REQ_T& request, ROS_SERVICE_RESP_T& response) {
@@ -61,8 +105,16 @@ private:
   }
 };
 
+class ROSServiceClientProxyBase : public ROSServiceProxyBase
+{
+private:
+  //! The underlying ROS service client
+  ros::ServiceClient client_;
+};
+
 template<class ROS_SERVICE_T, class ROS_SERVICE_REQ_T, class ROS_SERVICE_RESP_T>
-class ROSServiceClientProxy : public ROSServiceClientProxyBase {
+class ROSServiceClientProxy : public ROSServiceClientProxyBase 
+{
 public:
 
   ROSServiceClientProxy(RTT::TaskContext* task, const std::string &service_name) {
@@ -71,8 +123,7 @@ public:
   }
 
 private:
-    RTT::Operation<bool(ROS_SERVICE_REQ_T&, ROS_SERVICE_RESP_T&)> operation_;
-    ros::ServiceClient client_;
+  RTT::Operation<bool(ROS_SERVICE_REQ_T&, ROS_SERVICE_RESP_T&)> operation_;
 };
 
 
