faff94d2154c0426906b7cdc1999313229b303e8
Jonathan Bohren <jbo@jhu.edu>
2013-08-05 15:36:58 -0400

    Adding more error checking

--- typekits/rtt_std_srvs/src/rtt_ros_service_proxies.cpp
+++ typekits/rtt_std_srvs/src/rtt_ros_service_proxies.cpp
@@ -9,21 +9,33 @@
 #include <std_srvs/Empty.h>
 ////////////////////////////////////////////////////////////////////////////////
 
-bool registerROSServiceProxies(){
-  // Get the ros service service
-  RTT::Service::shared_ptr rss(RTT::internal::GlobalService::Instance()->getService("rosservice_registry"));
-  RTT::OperationCaller<void(ROSServiceProxyFactoryBase*)> register_service_factory = rss->getOperation("registerServiceFactory");
+bool registerROSServiceProxies() {
+  // Get the ros service registry service
+  if(!RTT::internal::GlobalService::Instance()->hasService("rosservice_registry")) {
+    return false;
+  }
+
+  // Get the factory registration operation
+  RTT::OperationCaller<bool(ROSServiceProxyFactoryBase*)> register_service_factory = 
+    RTT::internal::GlobalService::Instance()->getService("rosservice_registry")->getOperation("registerServiceFactory");
+
+  // Make sure the registration operation is ready
+  if(!register_service_factory.ready()) {
+    return false;
+  }
+
+  // True at the end if all factories have been registered
+  bool success = true;
 
   //////////////////////////////////////////////////////////////////////////////
-  /** Proxy for "std_srvs/Empty" **/
-  register_service_factory(new ROSServiceProxyFactory<std_srvs::Empty>("std_srvs/Empty"));
+  success = success && register_service_factory(new ROSServiceProxyFactory<std_srvs::Empty>("std_srvs/Empty"));
   //////////////////////////////////////////////////////////////////////////////
 
-  return true;
+  return success;
 }
 
 extern "C" {
-  bool loadRTTPlugin(RTT::TaskContext* c){ return registerROSServiceProxies(); }
-  std::string getRTTPluginName (){ return "rtt_std_srvs_ros_service_proxies"; }
-  std::string getRTTTargetName (){ return OROCOS_TARGET_NAME; }
+  bool loadRTTPlugin(RTT::TaskContext* c) { return registerROSServiceProxies(); }
+  std::string getRTTPluginName () { return "rtt_std_srvs_ros_service_proxies"; }
+  std::string getRTTTargetName () { return OROCOS_TARGET_NAME; }
 }
