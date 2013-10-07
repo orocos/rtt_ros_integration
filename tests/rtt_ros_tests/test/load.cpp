8b7f5cccae345eea10bd49c379acecbd0cb1536f
Jonathan Bohren <jbo@jhu.edu>
2013-10-07 09:26:51 -0400

    adding load test

---
+++ tests/rtt_ros_tests/test/load.cpp
@@ -0,0 +1,42 @@
+/** Copyright (c) 2013, Jonathan Bohren, all rights reserved. 
+ * This software is released under the BSD 3-clause license, for the details of
+ * this license, please see LICENSE.txt at the root of this repository. 
+ */
+
+#include <string>
+#include <vector>
+#include <iterator>
+
+#include <rtt/os/startstop.h>
+
+#include <ocl/DeploymentComponent.hpp>
+#include <ocl/TaskBrowser.hpp>
+#include <ocl/LoggingService.hpp>
+#include <rtt/Logger.hpp>
+#include <rtt/deployment/ComponentLoader.hpp>
+
+#include <boost/assign/std/vector.hpp>
+using namespace boost::assign;
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using ::testing::ElementsAre;
+
+TEST(DataFlowTest, Latchanalysis) 
+{
+  // Import rtt_ros plugin
+  RTT::ComponentLoader::Instance()->import("rtt_ros", "" );
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  // Initialize Orocos
+  __os_init(argc, argv);
+
+  RTT::Logger::log().setStdStream(std::cerr);
+  RTT::Logger::log().mayLogStdOut(true);
+  //RTT::Logger::log().setLogLevel(RTT::Logger::Info);
+
+  return RUN_ALL_TESTS();
+}
