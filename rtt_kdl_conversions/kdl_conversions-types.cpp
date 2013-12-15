de1cde79055626c88220ecc3263dd31719339c4e
Johannes Meyer <johannes@intermodalics.eu>
2013-12-15 15:49:50 +0100

    rtt_kdl_conversions: added new operations for methods added in kdl_conversions 1.9.7

--- rtt_kdl_conversions/kdl_conversions-types.cpp
+++ rtt_kdl_conversions/kdl_conversions-types.cpp
@@ -17,6 +17,20 @@ namespace KDL
       bool loadOperators()
       {
           RTT::Service::shared_ptr gs = RTT::internal::GlobalService::Instance();
+          gs->provides("KDL")->addOperation("pointMsgToKDL",&tf::pointMsgToKDL);
+          gs->provides("KDL")->addOperation("pointKDLToMsg",&tf::pointKDLToMsg);
+          gs->provides("KDL")->addOperation("poseMsgToKDL",&tf::poseMsgToKDL);
+          gs->provides("KDL")->addOperation("poseKDLToMsg",&tf::poseKDLToMsg);
+          gs->provides("KDL")->addOperation("quaternionMsgToKDL",&tf::quaternionMsgToKDL);
+          gs->provides("KDL")->addOperation("quaternionKDLToMsg",&tf::quaternionKDLToMsg);
+          gs->provides("KDL")->addOperation("transformMsgToKDL",&tf::transformMsgToKDL);
+          gs->provides("KDL")->addOperation("transformKDLToMsg",&tf::transformKDLToMsg);
+          gs->provides("KDL")->addOperation("twistMsgToKDL",&tf::twistMsgToKDL);
+          gs->provides("KDL")->addOperation("twistKDLToMsg",&tf::twistKDLToMsg);
+          gs->provides("KDL")->addOperation("vectorMsgToKDL",&tf::vectorMsgToKDL);
+          gs->provides("KDL")->addOperation("vectorKDLToMsg",&tf::vectorKDLToMsg);
+          gs->provides("KDL")->addOperation("wrenchMsgToKDL",&tf::wrenchMsgToKDL);
+          gs->provides("KDL")->addOperation("wrenchKDLToMsg",&tf::wrenchKDLToMsg);
           gs->provides("KDL")->addOperation("TwistToMsg",&tf::TwistKDLToMsg);
           gs->provides("KDL")->addOperation("MsgToTwist",&tf::TwistMsgToKDL);
           gs->provides("KDL")->addOperation("FrameToMsg",&tf::PoseKDLToMsg);
