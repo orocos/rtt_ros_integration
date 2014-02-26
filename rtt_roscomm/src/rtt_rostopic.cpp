b0422a3b44f821048f86002c6f79c0d4827e27a2
Jonathan Bohren <jbo@jhu.edu>
2014-02-26 15:51:14 -0500

    rtt_roscomm: Refactoring headers and global functions

---
+++ rtt_roscomm/src/rtt_rostopic.cpp
@@ -0,0 +1,36 @@
+
+#include <rtt_roscomm/rtt_rostopic.h>
+
+RTT::ConnPolicy rtt_roscomm::topic(const std::string& name) {
+  RTT::ConnPolicy cp = RTT::ConnPolicy::data();
+  cp.transport = protocol_id;
+  cp.name_id = name;
+  return cp;
+}
+
+/**
+ * Returns a ConnPolicy object for streaming to or from 
+ * the given ROS topic. Also specifies the buffer size of
+ * the connection to be created.
+ */
+RTT::ConnPolicy rtt_roscomm::topicBuffer(const std::string& name, int size) {
+  RTT::ConnPolicy cp = RTT::ConnPolicy::buffer(size);
+  cp.transport = protocol_id;
+  cp.name_id = name;
+  return cp;
+}
+
+/**
+ * Returns a ConnPolicy object for streaming to or from
+ * the given ROS topic. Use this only for unbuffered
+ * publishing, where the publish() method is called
+ * in the thread of the writing TaskContext.
+ */
+RTT::ConnPolicy rtt_roscomm::topicUnbuffered(const std::string& name) {
+  RTT::ConnPolicy cp = RTT::ConnPolicy();
+  cp.type = RTT::ConnPolicy::UNBUFFERED;
+  cp.transport = protocol_id;
+  cp.name_id = name;
+  return cp;
+}
+
--- rtt_roscomm/src/rtt_rostopic_service.cpp
+++ rtt_roscomm/src/rtt_rostopic_service.cpp
@@ -1,71 +1,43 @@
 #include <rtt/RTT.hpp>
 #include <rtt/internal/GlobalService.hpp>
-#include <rtt_rostopic/rtt_rostopic.h> 
+#include <rtt_roscomm/rtt_rostopic.h> 
 
 using namespace RTT;
 using namespace std;
 
-/**
- * The globally loadable ROS service.
- */
-class ROSTopicService {
-public:
-  static const int protocol_id;
-
-  /**
-   * Returns a ConnPolicy object for streaming to or from 
-   * the given ROS topic. No buffering is done.
-   */
-  static ConnPolicy topic(const std::string& name) {
-    ConnPolicy cp = ConnPolicy::data();
-    cp.transport = protocol_id;
-    cp.name_id = name;
-    return cp;
-  }
-
-  /**
-   * Returns a ConnPolicy object for streaming to or from 
-   * the given ROS topic. Also specifies the buffer size of
-   * the connection to be created.
-   */
-  static ConnPolicy topicBuffer(const std::string& name, int size) {
-    ConnPolicy cp = ConnPolicy::buffer(size);
-    cp.transport = protocol_id;
-    cp.name_id = name;
-    return cp;
-  }
-
-  /**
-   * Returns a ConnPolicy object for streaming to or from
-   * the given ROS topic. Use this only for unbuffered
-   * publishing, where the publish() method is called
-   * in the thread of the writing TaskContext.
-   */
-  static ConnPolicy topicUnbuffered(const std::string& name) {
-    ConnPolicy cp = ConnPolicy();
-    cp.type = ConnPolicy::UNBUFFERED;
-    cp.transport = protocol_id;
-    cp.name_id = name;
-    return cp;
-  }};
-
-const int ROSTopicService::protocol_id = ORO_ROS_PROTOCOL_ID;
-
 void loadROSTopicService(){
   RTT::Service::shared_ptr ros = RTT::internal::GlobalService::Instance()->provides("ros");
+  RTT::Service::shared_ptr roscomm = ros->provides("comm");
 
-  // ROS Topic-based Operations
-  ros->addOperation("topic", &ROSTopicService::topic).doc(
+  // New topic construction operators
+  roscomm->addConstant("protocol_id", rtt_roscomm::protocol_id);
+
+  roscomm->addOperation("topicDataConnPolicy", &rtt_roscomm::topic).doc(
       "Creates a ConnPolicy for subscribing to or publishing a topic. No buffering is done, only the last message is kept.").arg(
           "name", "The ros topic name");
-  ros->addOperation("topicBuffer", &ROSTopicService::topicBuffer).doc(
+
+  roscomm->addOperation("topicBufferedConnPolicy", &rtt_roscomm::topicBuffer).doc(
       "Creates a ConnPolicy for subscribing to or publishing a topic with a fixed-length message buffer.").arg(
           "name", "The ros topic name").arg(
           "size","The size of the buffer.");
-  ros->addOperation("topicUnbuffered", &ROSTopicService::topicUnbuffered).doc(
+
+  roscomm->addOperation("topicUnbufferedConnPolicy", &rtt_roscomm::topicUnbuffered).doc(
       "Creates a ConnPolicy for unbuffered publishing a topic. This may not be real-time safe!").arg(
           "name", "The ros topic name");
-  ros->addConstant("protocol_id", ROSTopicService::protocol_id);
+
+  // Backwards-compatibility
+  ros->addConstant("protocol_id", rtt_roscomm::protocol_id);
+
+  ros->addOperation("topic", &rtt_roscomm::topic).doc(
+      "Creates a ConnPolicy for subscribing to or publishing a topic. No buffering is done, only the last message is kept.").arg(
+          "name", "The ros topic name");
+  ros->addOperation("topicBuffer", &rtt_roscomm::topicBuffer).doc(
+      "Creates a ConnPolicy for subscribing to or publishing a topic with a fixed-length message buffer.").arg(
+          "name", "The ros topic name").arg(
+          "size","The size of the buffer.");
+  ros->addOperation("topicUnbuffered", &rtt_roscomm::topicUnbuffered).doc(
+      "Creates a ConnPolicy for unbuffered publishing a topic. This may not be real-time safe!").arg(
+          "name", "The ros topic name");
 }
 
 using namespace RTT;
