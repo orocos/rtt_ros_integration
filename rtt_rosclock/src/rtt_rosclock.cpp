e623b1bc741b104b18c6794ff7b4e203018584e7
Jonathan Bohren <jbo@jhu.edu>
2014-05-15 15:29:30 -0400

    Fixup before merge
    rtt_rosclock: using long-long literals instead of floats in time
    conversion
    rtt_rosdeployment: using wrong license that snuk in

--- rtt_rosclock/src/rtt_rosclock.cpp
+++ rtt_rosclock/src/rtt_rosclock.cpp
@@ -51,10 +51,12 @@ const ros::Time rtt_rosclock::host_wall_now()
 
 const ros::Time rtt_rosclock::rtt_now() 
 {
+  // count the zeros...   -987654321--
+  const uint64_t one_E9 = 1000000000ll;
   // NOTE: getNSecs returns wall time, getTicks returns offset time
   uint64_t nsec64 = RTT::os::TimeService::ticks2nsecs(RTT::os::TimeService::Instance()->getTicks()); //RTT::os::TimeService::Instance()->getNSecs();
-  uint32_t sec = nsec64 / 1E9;
-  uint32_t nsec = (uint32_t)(nsec64 - (sec*1E9));
+  uint32_t sec = nsec64 / one_E9;
+  uint32_t nsec = (uint32_t)(nsec64 - (sec*one_E9));
   return ros::Time(sec, nsec);
 }
 
