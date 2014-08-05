c20ac4e671ffd3c632f9243447fa748fc755a56d
Jonathan Bohren <jbo@jhu.edu>
2014-08-05 14:16:59 -0400

    rtt_actionlib: adding virtual destructor

--- rtt_actionlib/include/rtt_actionlib/rtt_action_server.h
+++ rtt_actionlib/include/rtt_actionlib/rtt_action_server.h
@@ -35,6 +35,8 @@ namespace rtt_actionlib {
 
     virtual ~RTTActionServerStatusTimer() { }
 
+    virtual ~RTTActionServerStatusTimer() { }
+
     //! Publish the action server status
     virtual void timeout(RTT::os::Timer::TimerId timer_id) {
       server_.publishStatus();
